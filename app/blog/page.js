5a17ad0761fd5371a4c96fe302c4ca0065e7c50b
yash00097 <yashwanthvuppala77@gmail.com>
2025-07-04 17:34:10 +0530

    Configuring Dynamic Routes & Using Route Parameters

---
+++ app/blog/page.js
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+function BlogPage() {
+    return ( 
+        <main>
+            <h1>Blog Post</h1>
+            <p><Link href="/blog/post-1">Post 1</Link></p>
+            <p><Link href="/blog/post-2">Post 2</Link></p>
+        </main>
+     );
+}
+
+export default BlogPage;
