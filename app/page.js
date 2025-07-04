6d97ed4e7e91340a2c8d1be39f746da5f5503fda
yash00097 <yashwanthvuppala77@gmail.com>
2025-07-04 15:50:48 +0530

    Navigating Between Pages - Wrong & Right Solution

--- app/page.js
+++ app/page.js
@@ -1,9 +1,13 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main>
       <img src="/logo.png" alt="A server surrounded by magic sparkles." />
       <h1>Welcome to this NextJS Course!</h1>
       <p>🔥 Let&apos;s get started! 🔥</p>
+      {/* <p><a href="/about">About US</a></p> is not a method for single page apps*/}
+      <p><Link href="/about">About US</Link></p>
     </main>
   );
 }
